0e5568b91d5dde1d762002f976134abd340fbcbf
KacperDrewniak <50110237+KacperDrewniak@users.noreply.github.com>
2019-12-26 03:11:13 +0100

    Add new Post

--- App/app/controllers/posts_controller.rb
+++ App/app/controllers/posts_controller.rb
@@ -4,11 +4,32 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
   end
 
   def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path  
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body, :published)
   end
 end
